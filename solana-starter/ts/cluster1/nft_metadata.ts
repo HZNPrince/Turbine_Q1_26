94f0971589b79b963fff631089b3579458754ea1
HZNPrince <hznprincework@gmail.com>
2026-01-21 11:59:41 +0530

    Hide wallet

--- solana-starter/ts/cluster1/nft_metadata.ts
+++ solana-starter/ts/cluster1/nft_metadata.ts
@@ -1,21 +1,26 @@
-import wallet from "../../TurHCThfhhm41FptMhG7C8daUBLaEjVQJnMZTybj9yi.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
-import { irysUploader } from "@metaplex-foundation/umi-uploader-irys"
+import wallet from "../../wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createGenericFile,
+  createSignerFromKeypair,
+  signerIdentity,
+} from "@metaplex-foundation/umi";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
 
 // Create a devnet connection
-const umi = createUmi("https://api.devnet.solana.com")
+const umi = createUmi("https://api.devnet.solana.com");
 
-let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet))
-const signer = createSignerFromKeypair(umi, keypair)
+let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
+const signer = createSignerFromKeypair(umi, keypair);
 
-umi.use(irysUploader({ address: "https://devnet.irys.xyz" }))
-umi.use(signerIdentity(signer))
-;(async () => {
+umi.use(irysUploader({ address: "https://devnet.irys.xyz" }));
+umi.use(signerIdentity(signer));
+(async () => {
   try {
     // Follow this JSON structure
     // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
-    const image = "https://gateway.irys.xyz/FAK7pRrab1a1EHt6BMfgw7EhuENc5TcheCnQvPYUSKh4"
+    const image =
+      "https://gateway.irys.xyz/FAK7pRrab1a1EHt6BMfgw7EhuENc5TcheCnQvPYUSKh4";
     const metadata = {
       name: "Rugy-Rug",
       symbol: "Rugby",
@@ -31,10 +36,10 @@ umi.use(signerIdentity(signer))
         ],
       },
       creators: [],
-    }
-    const myUri = await umi.uploader.uploadJson(metadata)
-    console.log("Your metadata URI: ", myUri)
+    };
+    const myUri = await umi.uploader.uploadJson(metadata);
+    console.log("Your metadata URI: ", myUri);
   } catch (error) {
-    console.log("Oops.. Something went wrong", error)
+    console.log("Oops.. Something went wrong", error);
   }
-})()
+})();
--- solana-starter/ts/cluster1/spl_mint.ts
+++ solana-starter/ts/cluster1/spl_mint.ts
@@ -1,32 +1,44 @@
 // Your ata is: E2Vdkec2e7XPCaykJqNb8AuWDPD8f9kRdW9fYEqLcwZ9
 // Your mint txid: G2uPAoQeLyuvhAgzoWHZvHrsnPazDq495rtsV8JEZDF4XURLhRJmi75fxMM4N3871xJ9yqJTBR3CcqGemgwF22u
 
-import { Keypair, PublicKey, Connection, Commitment } from "@solana/web3.js"
-import { getOrCreateAssociatedTokenAccount, mintTo } from "@solana/spl-token"
-import wallet from "../../TurHCThfhhm41FptMhG7C8daUBLaEjVQJnMZTybj9yi.json"
+import { Keypair, PublicKey, Connection, Commitment } from "@solana/web3.js";
+import { getOrCreateAssociatedTokenAccount, mintTo } from "@solana/spl-token";
+import wallet from "../../wallet.json";
 
 // Import our keypair from the wallet file
-const keypair = Keypair.fromSecretKey(new Uint8Array(wallet))
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
 //Create a Solana devnet connection
-const commitment: Commitment = "confirmed"
-const connection = new Connection("https://api.devnet.solana.com", commitment)
+const commitment: Commitment = "confirmed";
+const connection = new Connection("https://api.devnet.solana.com", commitment);
 
-const token_decimals = 1_000_000n
+const token_decimals = 1_000_000n;
 
 // Mint address
-const mint = new PublicKey("5hPgadD2pFvuehEaH91NXgRpmZpdghTgBcSeragdXSQq")
+const mint = new PublicKey("5hPgadD2pFvuehEaH91NXgRpmZpdghTgBcSeragdXSQq");
 
-;(async () => {
+(async () => {
   try {
     // Create an ATA
-    const ata = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, keypair.publicKey)
-    console.log(`Your ata is: ${ata.address.toBase58()}`)
+    const ata = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      keypair.publicKey,
+    );
+    console.log(`Your ata is: ${ata.address.toBase58()}`);
 
     // Mint to ATA
-    const mintTx = await mintTo(connection, keypair, mint, ata.address, keypair, 10000_000000)
-    console.log(`Your mint txid: ${mintTx}`)
+    const mintTx = await mintTo(
+      connection,
+      keypair,
+      mint,
+      ata.address,
+      keypair,
+      10000_000000,
+    );
+    console.log(`Your mint txid: ${mintTx}`);
   } catch (error) {
-    console.log(`Oops, something went wrong: ${error}`)
+    console.log(`Oops, something went wrong: ${error}`);
   }
-})()
+})();
